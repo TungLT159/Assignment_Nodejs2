8e615c2de5709acd57f311a32dd2197ecb62ed4a
TungLe <thanhtung12121212@gmail.com>
2022-07-30 16:07:56 +0700

    LAB02.3

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
     console.log('In another middleware')
+    res.send('<h1>Hello from Express!</h1>')
 })
 
 const server = http.createServer(app)

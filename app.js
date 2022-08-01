44352fdea510de680b1dc2a25d7c5522febcb222
TungLe <thanhtung12121212@gmail.com>
2022-08-01 15:04:07 +0700

    LAB2.6-LAB2.14

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const http = require('http');
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use('/add-product', (req, res, next) => {
-    console.log('In another middleware')
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/admin', adminRoutes)
+app.use(shopRoutes)
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
-app.use('/product', (req, res, next) => {
-    console.log(req.body)
-    res.redirect('/')
-})
-app.use('/', (req, res, next) => {
-    console.log('In another middleware')
-    res.send('<h1>Hello from Express!</h1>')
-})
+
 app.listen(3000);

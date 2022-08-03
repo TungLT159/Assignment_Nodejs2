8f23885b988761021b95e81437213dbd2ea4e835
TungLe <thanhtung12121212@gmail.com>
2022-08-03 15:19:47 +0700

    LAB03.7

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const http = require('http');
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars')
+
+
 const app = express();
-app.set('view engine', 'pug')
+
+app.engine('hbs', expressHbs())
+app.set('view engine', 'hbs')
 app.set('views', 'views')
 
 const adminData = require('./routes/admin')

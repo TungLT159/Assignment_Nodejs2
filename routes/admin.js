b8bc835c40d48e54e8776e3ec75c8a80b2874cf3
TungLe <thanhtung12121212@gmail.com>
2022-08-03 15:43:09 +0700

    LAB03.8

--- routes/admin.js
+++ routes/admin.js
@@ -6,7 +6,13 @@ const router = express.Router()
 const products = []
 
 router.get('/add-product', (req, res, next) => {
-    res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' })
+    res.render('add-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        activeAddProduct: true,
+        productCSS: true,
+        formsCSS: true
+    })
 })
 router.post('/add-product', (req, res, next) => {
     products.push({ title: req.body.title })

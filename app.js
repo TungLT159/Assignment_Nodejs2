8926049ee55d55ea8ba3477a2f51fdd177203196
TungLe <thanhtung12121212@gmail.com>
2022-08-04 15:32:37 +0700

    LAB04.2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const errorController = require('./controller/error')
 
 const app = express();
 
@@ -20,8 +20,6 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
-app.use((req, res, next) => {
-    res.status(404).render('404', { pageTitle: 'Page Not Found' })
-})
+app.use(errorController.get404)
 
 app.listen(3000);

a1c2c745ce96657304ff6a32b8e0d3e94f75a3ee
TungLe <thanhtung12121212@gmail.com>
2022-08-05 13:51:50 +0700

    LAB04.7

--- models/product.js
+++ models/product.js
@@ -1,34 +1,41 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 
 const p = path.join(
     path.dirname(process.mainModule.filename),
     'data',
     'products.json'
-)
-const getProductsFromFile = (cb) => {
+);
+
+const getProductsFromFile = cb => {
     fs.readFile(p, (err, fileContent) => {
         if (err) {
-            cb([])
+            cb([]);
         } else {
-            cb(JSON.parse(fileContent))
+            cb(JSON.parse(fileContent));
         }
-    })
-}
+    });
+};
 
 module.exports = class Product {
-    constructor(title) {
-        this.title = title
+    constructor(title, imageUrl, description, price) {
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
     }
+
     save() {
+        this.id = Math.random().toString()
         getProductsFromFile(products => {
-            products.push(this)
-            fs.writeFile(p, JSON.stringify(products), (err) => {
-                console.log(err)
-            })
-        })
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), err => {
+                console.log(err);
+            });
+        });
     }
+
     static fetchAll(cb) {
-        getProductsFromFile(cb)
+        getProductsFromFile(cb);
     }
-}
+};
--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,19 @@
 const path = require('path');
+
 const express = require('express');
-const productsController = require('../controller/products')
 
-const router = express.Router()
+const shopController = require('../controllers/shop');
 
-router.get('/', productsController.getProducts)
+const router = express.Router();
+
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
+
 module.exports = router;

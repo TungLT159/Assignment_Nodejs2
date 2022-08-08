62981f8bb283d8fc585949e04ba47c9242e61c2a
TungLe <thanhtung12121212@gmail.com>
2022-08-08 20:29:30 +0700

    LAB04.21

--- models/cart.js
+++ models/cart.js
@@ -38,6 +38,9 @@ module.exports = class Cart {
             if (err) return
             const updatedCart = {...JSON.parse(fileContent) }
             const product = updatedCart.products.find(prod => prod.id === id)
+            if (!product) {
+                return
+            }
             const productQty = product.qty
             updatedCart.products = updatedCart.products.filter(prod => prod.id !== id)
             updatedCart.totalPice = updatedCart.totalPice - productPrice * productQty

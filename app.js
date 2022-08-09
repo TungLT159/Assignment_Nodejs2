47664a657ef93db1908fb0ae0116c9a02e4a6f34
TungLe <thanhtung12121212@gmail.com>
2022-08-09 21:04:30 +0700

    LAB05.1

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 
+const db = require('./util/database')
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -13,6 +15,7 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+db.execute('SELECT * FROM products')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
---
+++ util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2')
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'dvalvuumm1ty1'
+})
+
+module.exports = pool.promise()

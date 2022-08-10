b081fe8a44290be0027dbfcf3c0422c834ec69e3
TungLe <thanhtung12121212@gmail.com>
2022-08-10 22:43:38 +0700

    LAB05.13

--- controllers/shop.js
+++ controllers/shop.js
@@ -16,16 +16,17 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
     const prodId = req.params.productId
-        // Product
-        //     .findById(prodId)
-        //     .then(([product]) => {
-        //         res.render('shop/product-detail', {
-        //             product: product[0],
-        //             pageTitle: product.title,
-        //             path: '/products'
-        //         })
-        //     })
-        //     .catch(err => console.log(err))
+
+    // Product
+    //     .findAll({ wher: { id: prodId } })
+    //     .then(products => {
+    //         res.render('shop/product-detail', {
+    //             product: products[0],
+    //             pageTitle: products[0].title,
+    //             path: '/products'
+    //         })
+    //     })
+    //     .catch(err => console.log(err))
 
     Product
         .findByPk(prodId)

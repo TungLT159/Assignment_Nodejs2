47f849c8eccd571a634a5acdd2e1bf76b49bc118
TungLe <thanhtung12121212@gmail.com>
2022-08-10 21:59:53 +0700

    LAB05.10

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 
-const db = require('./util/database')
+const sequelize = require('./util/database')
 
 const app = express();
 
@@ -23,4 +23,9 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize
+    .sync()
+    .then(result => {
+        app.listen(3000)
+    })
+    .catch(err => console.log(err))

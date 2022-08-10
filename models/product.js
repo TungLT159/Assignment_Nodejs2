1e96e5ef85b7f9419e61abe2f0bc2b828148946f
TungLe <thanhtung12121212@gmail.com>
2022-08-10 19:52:38 +0700

    LAB05.6

--- models/product.js
+++ models/product.js
@@ -14,7 +14,9 @@ module.exports = class Product {
     }
 
     save() {
-
+        return db.execute(
+            'INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)', [this.title, this.price, this.imageUrl, this.description]
+        )
     }
 
     static deleteById(id) {

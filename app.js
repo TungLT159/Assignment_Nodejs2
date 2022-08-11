d6e914a1e6494dadbdce91b1d9a2effc66511da4
TungLe <thanhtung12121212@gmail.com>
2022-08-11 20:30:21 +0700

    LAB05.18

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 
 const sequelize = require('./util/database')
+const Product = require('./models/product')
+const User = require('./models/user')
 
 const app = express();
 
@@ -18,14 +20,37 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    User.findByPk(1)
+        .then((user) => {
+            req.user = user
+            next()
+        })
+        .catch(err => console.log(err))
+})
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product)
 sequelize
+// .sync({ force: true })
     .sync()
     .then(result => {
+        return User.findByPk(1)
+        app.listen(3000)
+    })
+    .then(user => {
+        if (!user) {
+            return User.create({ name: 'tung', email: 'tung@gmail.com' })
+        }
+        return user
+    })
+    .then(user => {
+        // console.log(user)
         app.listen(3000)
     })
     .catch(err => console.log(err))

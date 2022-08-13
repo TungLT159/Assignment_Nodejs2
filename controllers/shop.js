93e3fc47ee24a373c34ae9b97fc739f85eda4f32
TungLe <thanhtung12121212@gmail.com>
2022-08-13 19:47:13 +0700

    LAB05.27

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require('../models/product');
-const Cart = require('../models/cart');
+const Order = require('../models/order');
 
 exports.getProducts = (req, res, next) => {
     Product.findAll()
@@ -119,6 +119,29 @@ exports.postCartDeleteProduct = (req, res, next) => {
         })
         .catch(err => console.log(err))
 };
+
+exports.postOrder = (req, res, next) => {
+    req.user
+        .getCart()
+        .then(cart => {
+            return cart.getProducts()
+        })
+        .then(products => {
+            return req.user
+                .createOrder()
+                .then(order => {
+                    return order.addProducts(products).map(product => {
+                        product.orderItem = { quantity: product.cartItem.quantity }
+                        return product
+                    })
+                })
+                .catch(err => console.log(err))
+        })
+        .then(result => {
+            res.redirect('/orders')
+        })
+        .catch(err => console.log(err))
+}
 
 exports.getOrders = (req, res, next) => {
     res.render('shop/orders', {

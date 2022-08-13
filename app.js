faf19e6945146fd66687052c7b86cc0f24af0843
TungLe <thanhtung12121212@gmail.com>
2022-08-13 16:22:00 +0700

    LAB05.22

--- app.js
+++ app.js
@@ -44,8 +44,8 @@ Cart.belongsToMany(Product, { through: CartItem })
 Product.belongsToMany(Cart, { through: CartItem })
 
 sequelize
-    .sync({ force: true })
-    // .sync()
+// .sync({ force: true })
+    .sync()
     .then(result => {
         return User.findByPk(1)
         app.listen(3000)
@@ -58,6 +58,9 @@ sequelize
     })
     .then(user => {
         // console.log(user)
+        return user.createCart()
+    })
+    .then(cart => {
         app.listen(3000)
     })
     .catch(err => console.log(err))

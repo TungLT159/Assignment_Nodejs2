03b58100b9e60cd8f3c068d0d086578c5eb25a75
TungLe <thanhtung12121212@gmail.com>
2022-08-14 15:09:35 +0700

    LAB06.1

--- util/database.js
+++ util/database.js
@@ -1,8 +1,14 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
 
-const sequelize = new Sequelize('node-complete', 'root', 'dvalvuumm1ty1', {
-    dialect: 'mysql',
-    host: 'localhost'
-});
+const mongoConnect = (callback) => {
+    MongoClient
+        .connect('mongodb+srv://tunglt:dvalvuumm1ty1@cluster0.5hjpvkp.mongodb.net/?retryWrites=true&w=majority')
+        .then(clinet => {
+            console.log('Connected')
+            callback(clinet)
+        })
+        .catch(err => console.log(err))
+}
 
-module.exports = sequelize;
+module.exports = mongoConnect

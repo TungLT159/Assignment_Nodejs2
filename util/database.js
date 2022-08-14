a981f90b14e4d751187d7e5b2ca76eacf4668498
TungLe <thanhtung12121212@gmail.com>
2022-08-14 15:33:21 +0700

    LAB06.3

--- util/database.js
+++ util/database.js
@@ -1,14 +1,28 @@
 const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient
 
+let _db
+
 const mongoConnect = (callback) => {
     MongoClient
-        .connect('mongodb+srv://tunglt:dvalvuumm1ty1@cluster0.5hjpvkp.mongodb.net/?retryWrites=true&w=majority')
+        .connect('mongodb+srv://tunglt:dvalvuumm1ty1@cluster0.5hjpvkp.mongodb.net/shop?retryWrites=true&w=majority')
         .then(clinet => {
             console.log('Connected')
+            _db = clinet.db()
             callback(clinet)
         })
-        .catch(err => console.log(err))
+        .catch(err => {
+            console.log(err)
+            throw err
+        })
 }
 
-module.exports = mongoConnect
+const getDb = () => {
+    if (_db) {
+        return _db
+    }
+    throw 'No database found!'
+}
+
+exports.mongoConnect = mongoConnect
+exports.getDb = getDb

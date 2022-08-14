879d97eb41c8777e24f5d3b68fba6d56873ff97e
TungLe <thanhtung12121212@gmail.com>
2022-08-14 15:43:56 +0700

    LAB06.4

--- controllers/admin.js
+++ controllers/admin.js
@@ -30,29 +30,29 @@ exports.postAddProduct = (req, res, next) => {
         });
 };
 
-exports.getEditProduct = (req, res, next) => {
-    const editMode = req.query.edit;
-    if (!editMode) {
-        return res.redirect('/');
-    }
-    const prodId = req.params.productId;
-    req.user
-        .getProducts({ where: { id: prodId } })
-        // Product.findById(prodId)
-        .then(products => {
-            const product = products[0];
-            if (!product) {
-                return res.redirect('/');
-            }
-            res.render('admin/edit-product', {
-                pageTitle: 'Edit Product',
-                path: '/admin/edit-product',
-                editing: editMode,
-                product: product
-            });
-        })
-        .catch(err => console.log(err));
-};
+// exports.getEditProduct = (req, res, next) => {
+//     const editMode = req.query.edit;
+//     if (!editMode) {
+//         return res.redirect('/');
+//     }
+//     const prodId = req.params.productId;
+//     req.user
+//         .getProducts({ where: { id: prodId } })
+//         // Product.findById(prodId)
+//         .then(products => {
+//             const product = products[0];
+//             if (!product) {
+//                 return res.redirect('/');
+//             }
+//             res.render('admin/edit-product', {
+//                 pageTitle: 'Edit Product',
+//                 path: '/admin/edit-product',
+//                 editing: editMode,
+//                 product: product
+//             });
+//         })
+//         .catch(err => console.log(err));
+// };
 
 exports.postEditProduct = (req, res, next) => {
     const prodId = req.body.productId;
--- models/product.js
+++ models/product.js
@@ -1,3 +1,5 @@
+const { name } = require('ejs');
+
 const getDb = require('../util/database').getDb
 
 class Product {
@@ -8,30 +10,16 @@ class Product {
         this.imageUrl = imageUrl;
     }
     save() {
-
+        const db = getDb()
+        db.collection('products')
+            .insertOne(this)
+            .then(result => {
+                console.log(result)
+            })
+            .catch((err) => console.log(err))
     }
 }
 
-const Product = sequelize.define('product', {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-    },
-    title: Sequelize.STRING,
-    price: {
-        type: Sequelize.DOUBLE,
-        allowNull: false
-    },
-    imageUrl: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    description: {
-        type: Sequelize.STRING,
-        allowNull: false
-    }
-});
+
 
 module.exports = Product;

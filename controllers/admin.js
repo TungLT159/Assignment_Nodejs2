104a8595620e833284704db6c10eb0d160724378
TungLe <thanhtung12121212@gmail.com>
2022-08-15 20:00:57 +0700

    LAB06.11

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,6 +1,5 @@
 const mongodb = require('mongodb')
 const Product = require('../models/product');
-const ObjectId = mongodb.ObjectId
 
 
 exports.getAddProduct = (req, res, next) => {
@@ -63,7 +62,7 @@ exports.postEditProduct = (req, res, next) => {
         updatedPrice,
         updatedDesc,
         updatedImageUrl,
-        new ObjectId(prodId)
+        prodId
     )
     product
         .save()

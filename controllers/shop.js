6c4fe4a83bfbb09f7f01654eaaa9367966f0c3c0
TungLe <thanhtung12121212@gmail.com>
2022-08-16 17:47:18 +0700

    LAB06.21

--- controllers/shop.js
+++ controllers/shop.js
@@ -76,50 +76,14 @@ exports.postCart = (req, res, next) => {
             res.redirect('/cart')
         })
         .catch(err => console.log(err))
-        // let fetchedCart;
-        // let newQuantity = 1;
-        // req.user
-        //     .getCart()
-        //     .then(cart => {
-        //         fetchedCart = cart;
-        //         return cart.getProducts({ where: { id: prodId } });
-        //     })
-        //     .then(products => {
-        //         let product;
-        //         if (products.length > 0) {
-        //             product = products[0];
-        //         }
-
-    //         if (product) {
-    //             const oldQuantity = product.cartItem.quantity;
-    //             newQuantity = oldQuantity + 1;
-    //             return product;
-    //         }
-    //         return Product.findByPk(prodId);
-    //     })
-    //     .then(product => {
-    //         return fetchedCart.addProduct(product, {
-    //             through: { quantity: newQuantity }
-    //         });
-    //     })
-    //     .then(() => {
-    //         res.redirect('/cart');
-    //     })
-    //     .catch(err => console.log(err));
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
     req.user
-        .getCart()
-        .then(cart => {
-            return cart.getProducts({ where: { id: prodId } })
-        })
-        .then(products => {
-            const product = products[0]
-            return product.cartItem.destroy()
-        })
+        .deleteItemFromCart(prodId)
         .then(result => {
+            console.log('Deleted')
             res.redirect('/cart')
         })
         .catch(err => console.log(err))
--- models/user.js
+++ models/user.js
@@ -56,6 +56,14 @@ class User {
             .catch(err => console.log(err))
     }
 
+    deleteItemFromCart(productId) {
+        const updatedCartItems = this.cart.items.filter(item => item.productId.toString() !== productId.toString())
+        const db = getDb()
+        return db
+            .collection('users')
+            .updateOne({ _id: new mongodb.ObjectId(this._id) }, { $set: { cart: { items: updatedCartItems } } })
+    }
+
     static findById(userId) {
         const db = getDb()
         return db

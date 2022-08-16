15f674a440c17d6bd2aa5f91ae2846f712ca3531
TungLe <thanhtung12121212@gmail.com>
2022-08-16 14:51:45 +0700

    LAB06.17

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
     User.findById("62fa59f603fcd98e120580c5")
         .then(user => {
-            req.user = user;
+            req.user = new User(user.name, user.email, user.cart, user._id);
             next();
         })
         .catch(err => console.log(err));
--- models/user.js
+++ models/user.js
@@ -20,7 +20,7 @@ class User {
         // const cartProduct = this.cart.items.findIndex(cp => {
         //     return cp._id === product._id
         // })
-        const updatedCart = { items: [{...product, quantity: 1 }] }
+        const updatedCart = { items: [{ productId: new mongodb.ObjectId(product._id), quantity: 1 }] }
         const db = getDb()
         return db
             .collection('users')

6e4aaecf29f4705eb827098b0d69961d260a687a
TungLe <thanhtung12121212@gmail.com>
2022-08-16 14:26:47 +0700

    LAB06.16

--- models/user.js
+++ models/user.js
@@ -2,9 +2,11 @@ const mongodb = require('mongodb')
 const getDb = require('../util/database').getDb
 
 class User {
-    constructor(username, email) {
+    constructor(username, email, cart, id) {
         this.username = username;
         this.email = email;
+        this.cart = cart;
+        this._id = id;
     }
 
     save() {
@@ -12,6 +14,17 @@ class User {
         return db
             .collection('users')
             .insertOne(this)
+    }
+
+    addToCart(product) {
+        // const cartProduct = this.cart.items.findIndex(cp => {
+        //     return cp._id === product._id
+        // })
+        const updatedCart = { items: [{...product, quantity: 1 }] }
+        const db = getDb()
+        return db
+            .collection('users')
+            .updateOne({ _id: new mongodb.ObjectId(this._id) }, { $set: { cart: updatedCart } })
     }
 
     static findById(userId) {

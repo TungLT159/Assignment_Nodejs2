f40a089d625f4db2e5421dcea86b5625817bb7e0
TungLe <thanhtung12121212@gmail.com>
2022-08-17 19:35:41 +0700

    LAB07.17

--- controllers/shop.js
+++ controllers/shop.js
@@ -109,10 +109,10 @@ exports.postOrder = (req, res, next) => {
 }
 
 exports.getOrders = (req, res, next) => {
-    req.user
-        .getOrders()
+    Order
+        .find({ 'user.userId': req.user._id })
         .then(orders => {
-            console.log(orders)
+            console.log('order::', orders[0].products)
             res.render('shop/orders', {
                 path: '/orders',
                 pageTitle: 'Your Orders',

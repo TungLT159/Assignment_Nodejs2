773329c47a714e3398666ea0d810b8f7f2664bb6
TungLe <thanhtung12121212@gmail.com>
2022-08-17 14:35:37 +0700

    LAB07.2

--- models/product.js
+++ models/product.js
@@ -1,3 +1,31 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const productSchema = new Schema({
+    title: {
+        type: String,
+        require: true
+    },
+    price: {
+        type: Number,
+        require: true
+    },
+    description: {
+        type: String,
+        require: true
+    },
+    imageUrl: {
+        type: String,
+        require: true
+    }
+})
+
+
+
+
+
+
 // const mongodb = require('mongodb')
 //     // const getDb = require('../util/database').getDb
 

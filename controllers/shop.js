ca5b57ef63a132b8e8d583fc9ee7de52538b65d1
TungLe <thanhtung12121212@gmail.com>
2022-08-17 15:57:43 +0700

    LAB07.4

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,8 +2,9 @@ const Product = require('../models/product');
 // const Order = require('../models/order');
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll()
+    Product.find()
         .then(products => {
+            console.log(products)
             res.render('shop/product-list', {
                 prods: products,
                 pageTitle: 'All Products',
@@ -38,7 +39,7 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-    Product.fetchAll()
+    Product.find()
         .then(products => {
             res.render('shop/index', {
                 prods: products,

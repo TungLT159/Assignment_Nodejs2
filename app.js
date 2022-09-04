18bea1bbe6aaba3cfb0ce904488fe130b24408c6
TungLe <thanhtung12121212@gmail.com>
2022-09-04 11:31:54 +0700

    Assignment_1

--- app.js
+++ app.js
@@ -1,51 +1,58 @@
 const path = require('path');
 
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const User = require('./models/user')
-
+const Staff = require('./models/staff')
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const staffRoutes = require('./routes/staff');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Luu staff vao req
 app.use((req, res, next) => {
-    User.findById("62fcb4b968eacff4ab40627b")
-        .then(user => {
-            req.user = user
+    Staff.findById("63121f26fccfb53d499124d7")
+        .then(staff => {
+            req.staff = staff
             next();
         })
         .catch(err => console.log(err));
 });
 
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+app.use(staffRoutes);
 
 app.use(errorController.get404);
 
 
 mongoose
-    .connect('mongodb+srv://tunglt:dvalvuumm1ty1@cluster0.5hjpvkp.mongodb.net/shop?retryWrites=true&w=majority')
+    .connect('mongodb+srv://tunglt:dvalvuumm1ty1@cluster0.5hjpvkp.mongodb.net/staff?retryWrites=true&w=majority')
     .then(result => {
-        User.findOne().then(user => {
-            if (!user) {
-                const user = new User({
-                    name: 'Tunglt',
-                    email: 'tunglt@gmail.com',
-                    cart: {
-                        items: []
-                    }
+        Staff.findOne().then(staff => {
+            if (!staff) {
+                //Neu chua co staff thi khoi tao 1 staff moi
+                const staff = new Staff({
+                    name: 'Nguyễn Văn A',
+                    doB: '1/1/2000',
+                    salaryScale: 1.1,
+                    startDate: '12/2/2019',
+                    department: 'IT',
+                    annualLeave: 4,
+                    imageUrl: 'https://www.seekpng.com/png/full/356-3562377_personal-user.png',
+                    sessionWork: [
+                        []
+                    ],
+                    isWork: false,
+                    totalHourWork: 0,
+                    onLeave: {},
+                    endDayWork: false
                 })
-                user.save()
+                staff.save()
             }
         })
         app.listen(3000)
---
+++ controllers/staff.js
@@ -0,0 +1,298 @@
+const Staff = require('../models/staff')
+const Covid = require('../models/covid')
+const mongoose = require('mongoose')
+
+exports.getStaff = (req, res, next) => {
+    res.render('viewStaff/staff-info', {
+        pageTitle: 'Thông Tin Nhân Viên',
+        path: '/staff',
+        staff: req.staff,
+        doB: `${req.staff.doB.getDate()}/${req.staff.doB.getMonth() + 1}/${req.staff.doB.getFullYear()}`,
+        startDate: `${req.staff.startDate.getDate()}/${req.staff.startDate.getMonth() + 1}/${req.staff.startDate.getFullYear()}`
+    });
+};
+
+exports.getIndex = (req, res, next) => {
+    res.render('viewStaff/index', {
+            pageTitle: 'Điểm Danh',
+            path: '/',
+            staff: req.staff,
+            isWork: req.staff.isWork,
+            totalHours: req.staff.totalHours,
+            messageErr: null
+        })
+        // res.send('hello')
+};
+
+exports.postImage = (req, res, next) => {
+    const imageUrl = req.body.imageUrl
+    Staff
+        .findById(req.staff._id)
+        .then(staff => {
+            //Capj nhat url anh
+            staff.imageUrl = imageUrl
+            return staff.save()
+        })
+        .then(result => {
+            return res.redirect('/staff')
+        })
+        .catch(err => console.log(err))
+}
+
+exports.getWork = (req, res, next) => {
+
+    return res.render('viewStaff/woking-hours-info', {
+        pageTitle: 'Thông tin giờ làm',
+        path: '/work',
+        staff: req.staff,
+        sessionWork: req.staff.sessionWork.items,
+        salary: 0,
+        errMessage: null,
+        monthSalary: null,
+        totalOTInMonth: 0,
+        missingHours: 0
+    })
+}
+
+exports.postWork = (req, res) => {
+    const item = req.staff.sessionWork.length - 1
+    const itemIndex = req.staff.sessionWork[item].length - 1
+    const monthSalary = req.body.monthSalary
+    const monthSalarySelected = req.staff.sessionWork[item][itemIndex].timeStart.getMonth() + 1
+    let salary = 0
+    let errMessage
+        // Tinh tong so gio OT
+    let totalOTInMonth = req.staff.sessionWork.reduce((acc, cur) => {
+            return acc + cur[cur.length - 1].overTime
+        }, 0)
+        //Tinh tong so gio lam thieu
+    let missingHours = req.staff.sessionWork.reduce((acc, cur) => {
+            let missHour = cur[cur.length - 1].totalHourWork <= 8 ? 8 - cur[cur.length - 1].totalHourWork : 0
+            return acc + missHour
+        }, 0)
+        //Kiem tra thang chon xem da co thong tin chua
+    if (monthSalary == monthSalarySelected) {
+        salary = req.staff.salaryScale * 3000000 + (totalOTInMonth - missingHours) * 200000
+    } else {
+        errMessage = 'Chưa có thông tin lương của tháng này.'
+    }
+
+    return res.render('viewStaff/woking-hours-info', {
+        pageTitle: 'Thông tin giờ làm',
+        path: '/work',
+        staff: req.staff,
+        sessionWork: req.staff.sessionWork.items,
+        salary: salary,
+        errMessage: errMessage,
+        monthSalary: monthSalary,
+        totalOTInMonth: totalOTInMonth,
+        missingHours: missingHours
+    })
+}
+
+exports.getCovid = (req, res, next) => {
+    Covid.find({ 'staff.staffId': req.staff._id })
+        .then(covidData => {
+            if (!covidData) {
+                throw new Error('Can not found covid info')
+            }
+            const covid = covidData[0]
+            if (covid.dateInfection === null) {
+                covid.dateInfection = new Date()
+            }
+            return res.render('viewStaff/covid-info', {
+                pageTitle: 'Thông tin covid',
+                path: '/covid',
+                staff: req.staff,
+                covid: covid,
+                dateVaccine1: `${covid.dateVaccine1.getDate()}/${covid.dateVaccine1.getMonth() + 1}/${covid.dateVaccine1.getFullYear()}`,
+                dateVaccine2: `${covid.dateVaccine2.getDate()}/${covid.dateVaccine2.getMonth() + 1}/${covid.dateVaccine2.getFullYear()}`,
+                dateTemp: `${covid.dateTemp.getHours()}:${covid.dateTemp.getMinutes()} - ${covid.dateTemp.getDate()}/${covid.dateTemp.getMonth() + 1}/${covid.dateVaccine2.getFullYear()}`,
+                dateInfection: `${covid.dateInfection.getDate()}/${covid.dateInfection.getMonth() + 1}/${covid.dateInfection.getFullYear()}`
+            })
+        })
+        .catch(err => console.log(err))
+}
+
+exports.postCovid = (req, res, next) => {
+    const temp = req.body.temp
+    const dateTemp = new Date().toISOString()
+    const dateVaccine1 = req.body.dateVaccine1
+    const typeVaccine1 = req.body.typeVaccine1
+    const dateVaccine2 = req.body.dateVaccine2
+    const typeVaccine2 = req.body.typeVaccine2
+    const dateInfection = req.body.dateInfection
+    const place = req.body.place
+    Covid.find({ 'staff.staffId': req.staff._id })
+        .then(covidData => {
+            //kiem tra da co du lieu covid hay chua
+            if (covidData.length === 0) {
+                const covid = new Covid({
+                    temp: temp,
+                    dateTemp: new Date(dateTemp),
+                    dateVaccine1: dateVaccine1,
+                    typeVaccine1: typeVaccine1,
+                    dateVaccine2: dateVaccine2,
+                    typeVaccine2: typeVaccine2,
+                    dateInfection: dateInfection,
+                    place: place,
+                    staff: {
+                        name: req.staff.name,
+                        staffId: req.staff
+                    }
+                })
+                return covid.save()
+            } else {
+                const covid = covidData[0]
+                covid.temp = temp
+                covid.dateTemp = new Date(dateTemp)
+                covid.dateVaccine1 = dateVaccine1
+                covid.typeVaccine1 = typeVaccine1
+                covid.dateVaccine2 = dateVaccine2
+                covid.typeVaccine2 = typeVaccine2
+                covid.dateInfection = dateInfection
+                covid.place = place
+                covid.staff.name = req.staff.name
+                covid.staff.staffId = req.staff
+                return covid.save()
+            }
+        })
+        .then(() => {
+            return res.redirect('/covid')
+        })
+        .catch(err => console.log(err))
+
+}
+
+exports.postWorking = (req, res) => {
+    const timeStart = new Date().toISOString()
+    const name = req.staff.name
+    const placeWork = req.body.placeWork
+    Staff
+        .findById(req.staff._id)
+        .then(staff => {
+            staff.endDayWork = false
+            staff.isWork = true
+            const item = []
+            const itemSection = {
+                timeStart: new Date(timeStart),
+                name: name,
+                placeWork: placeWork,
+            }
+            item.push(itemSection)
+                //Neu chua co ngay lam nao thi push vao phan tu dau tien
+            if (staff.sessionWork[staff.sessionWork.length - 1][0] === undefined) {
+                staff.sessionWork[staff.sessionWork.length - 1].push(itemSection)
+                return staff.save()
+            }
+            //Neu ngay thay doi thi push mang moi vao sessionWork
+            if (itemSection.timeStart.getDate() != staff.sessionWork[staff.sessionWork.length - 1][0].timeStart.getDate()) {
+                //Reset tong gio lam va lich off khi chuyen sang ngay moi
+                staff.totalHours = 0
+                staff.onLeave = {}
+                staff.sessionWork.push(item)
+                return staff.save()
+            }
+            //Neu ngay khong thay doi thi push object moi vao mang cua ngay lam do
+            staff.sessionWork[staff.sessionWork.length - 1].push(itemSection)
+            return staff.save()
+
+
+        })
+        .then(result => {
+            return res.redirect('/')
+        })
+        .catch(err => console.log(err))
+}
+
+exports.postEndWorking = (req, res) => {
+    const timeEnd = new Date().toISOString()
+    Staff
+        .findById(req.staff._id)
+        .then(staff => {
+            //Lay ra object cuoi cung cua sessionWork[item[]]
+            const item = staff.sessionWork.length - 1
+            const itemIndex = staff.sessionWork[item].length - 1
+            let totalTimeWork = new Date((new Date(timeEnd).getTime()) - (staff.sessionWork[item][itemIndex].timeStart.getTime()))
+                //Chuyen doi tu miliseconds sang hours
+            totalTimeWork = (totalTimeWork / 1000) / 3600
+
+            console.log(totalTimeWork)
+            staff.isWork = false
+            staff.totalHours += +totalTimeWork.toFixed(1)
+            staff.sessionWork[item][itemIndex].totalHour = totalTimeWork.toFixed(1)
+            staff.sessionWork[item][itemIndex].timeEnd = new Date(timeEnd)
+                // staff.sessionWork = [[]]
+            return staff.save()
+        })
+        .then(result => {
+            return res.redirect('/')
+        })
+        .catch(err => console.log(err))
+
+}
+
+exports.postEndDayWork = (req, res) => {
+    Staff
+        .findById(req.staff._id)
+        .then(staff => {
+            const item = staff.sessionWork.length - 1
+            const itemIndex = staff.sessionWork[item].length - 1
+            staff.sessionWork[item][itemIndex].totalHourWork = staff.totalHours
+            staff.sessionWork[item][itemIndex].hourAnnualLeave = staff.onLeave.hourAnnualLeave
+                //Check neu tong gio lam cua ngay lon hon 8 thi tinh vao gio OT
+            if (staff.totalHours > 8) {
+                staff.sessionWork[item][itemIndex].overTime = staff.totalHours - 8
+            } else {
+                staff.sessionWork[item][itemIndex].overTime = 0
+            }
+            return staff.save()
+
+        })
+        .then(() => {
+            res.redirect('/')
+        })
+        .catch(err => console.log(err))
+}
+
+exports.postOffWork = (req, res) => {
+    const dateStart = new Date(req.body.dateStart)
+    const dateEnd = new Date(req.body.dateEnd)
+    const reason = req.body.reason
+    const hourAnnualLeave = req.body.hourAnnualLeave
+    Staff
+        .findById(req.staff._id)
+        .then(staff => {
+            let messageErr
+                //Check truong hop so ngay dang ky lon hon so ngay nghi con lai
+            if ((dateEnd.getDate() - dateStart.getDate()) > staff.annualLeave) {
+                messageErr = 'Số ngày đăng ký vượt quá ngày nghỉ cho phép.'
+                return res.status(422).render('viewStaff/index', {
+                    pageTitle: 'Điểm Danh',
+                    path: '/',
+                    staff: req.staff,
+                    isWork: req.staff.isWork,
+                    totalHours: req.staff.totalHours,
+                    messageErr: messageErr
+                })
+            }
+            // Check truong hop nghi 1 ngay
+            if (dateEnd.getDate() == dateStart.getDate()) {
+                staff.annualLeave = hourAnnualLeave <= 4 ? staff.annualLeave - 0.5 : staff.annualLeave - 1
+            }
+            //Check truong hop nghi nhieu ngay
+            if (dateEnd.getDate() > dateStart.getDate()) {
+                const hourNum = hourAnnualLeave <= 4 ? 0.5 : 1
+                staff.annualLeave = staff.annualLeave - ((dateEnd.getDate() - dateStart.getDate()) + hourNum)
+            }
+            staff.onLeave.dateStart = dateStart
+            staff.onLeave.dateEnd = dateEnd
+            staff.onLeave.reason = reason
+            staff.onLeave.hourAnnualLeave = hourAnnualLeave
+            staff.save()
+                .then(() => {
+                    res.redirect('/')
+                })
+        })
+        .catch(err => console.log(err))
+}
